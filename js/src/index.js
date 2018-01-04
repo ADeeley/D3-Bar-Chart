6aed15b9730f083e51b4d1325df5f46f9056a370
Adam M Deeley <ad.deeley@gmail.com>
2018-01-04 20:38:52 +0000

    Initial commit

--- js/src/index.js
+++ js/src/index.js
@@ -1,8 +1,25 @@
 window.onload = function() {
-    let content = document.createElement("div"),
-        para = document.createElement("p"),
-        body = document.getElementsByTagName("body")[0];
+    d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(stats) {
+        let data = stats.data,
+            height = 600,
+            barWidth = 3;
 
-    para.innerHTML = "Hello, World!",
-    body.appendChild(content.appendChild(para));
+        let y = d3.scaleLinear()
+            .domain([0, Math.max.apply(null, (data.map((num) => num[1])))])
+            .range([0, height])
+
+        let chart = d3.select('.chart')
+            .attr('width', barWidth * data.length)
+            .attr('height', height);
+
+        let bar = chart.selectAll('g')
+            .data(data)
+        .enter().append('g')
+            .attr('transform', (d, i) => 'translate(0' + i * barWidth + ')');
+
+        bar.append('rect')
+            .attr('width', barWidth - 1)
+            .attr('height', (d) => y(d[1]) + 'px')
+            .attr('y', (d) => height - y(d[1]))
+    })
 }
